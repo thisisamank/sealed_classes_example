422fb06aa1f825c7393c688f579e063329e00646
Aman Kumar <thisisamank@gmail.com>
2023-05-01 00:45:22 +0530

    todo model added

---
+++ lib/models/todo.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+class Todo {
+  final int userId;
+  final int id;
+  final String title;
+  final bool completed;
+
+  Todo({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.completed,
+  });
+
+  factory Todo.fromJson(String str) => Todo.fromMap(json.decode(str));
+
+  String toJson() => json.encode(toMap());
+
+  factory Todo.fromMap(Map<String, dynamic> json) => Todo(
+        userId: json["userId"],
+        id: json["id"],
+        title: json["title"],
+        completed: json["completed"],
+      );
+
+  Map<String, dynamic> toMap() => {
+        "userId": userId,
+        "id": id,
+        "title": title,
+        "completed": completed,
+      };
+}
