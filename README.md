e92226109352840bbadf7b7cf90a9af4917fcb04
Aman Kumar <thisisamank@gmail.com>
2023-05-01 18:22:09 +0530

    readme updated

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-# dart_3
-
-A new Flutter project.
+This Github repository contains the source code for the blog post titled "Using Sealed Classes in Dart". In this blog post, we explore the benefits of sealed classes in Dart and provide some real-life examples of how to use them.
 
 ## Getting Started
+To get started with this code, you will need to have Flutter installed on your machine. Once Flutter is installed, you can clone this repository and open it in your favorite code editor.
+You will have to switch to flutter beta channel to run the code. To do so, run the following command in your terminal:
+```bash
+flutter channel beta
+```
+Then, run the following command to upgrade your Flutter installation:
+```bash
+flutter upgrade
+```
 
-This project is a starting point for a Flutter application.
 
-A few resources to get you started if this is your first Flutter project:
+## Code Overview
+The code in this repository is organized into several files, each serving a different purpose. Here is a brief overview of the different files:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+`endpoints.dart`: This file contains the API endpoint URL for the Todo app.
+`either.dart`: This file contains the implementation of the Either class, which is used to handle error cases.
+`todo_repository.dart`: This file contains the TodoRepository class, which fetches data from the API and returns it as an Either type.
+`future_value.dart`: This file contains the FutureValue class, which is used to safely manipulate asynchronous data.
+`todo_notifier.dart`: This file contains the TodoNotifier class, which manages the state of the API request.
+`todo_screen.dart`: This file contains the TodoScreen class, which displays the list of Todo items.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Conclusion
+We hope that you find this code and blog post helpful in understanding sealed classes in Dart. If you have any questions or comments, please feel free to open an issue on this repository. Thank you for reading!
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
-import 'package:dart_3/models/todo.dart';
-import 'package:dart_3/notifiers/todo_notifier.dart';
-import 'package:dart_3/repository/todo_repository.dart';
 import 'package:flutter/material.dart';
 
-import 'models/future_values.dart';
+import 'todo_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,79 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const TodoScreen(),
-    );
-  }
-}
-
-class TodoScreen extends StatefulWidget {
-  const TodoScreen({super.key});
-
-  @override
-  State<TodoScreen> createState() => _TodoScreenState();
-}
-
-class _TodoScreenState extends State<TodoScreen> {
-  final _todoNotifier = TodoNotifier(TodoRepository());
-  @override
-  void initState() {
-    _todoNotifier.getTodos();
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dart 3 : Todo app"),
-      ),
-      body: ValueListenableBuilder<FutureValues<List<Todo>>>(
-          valueListenable: _todoNotifier,
-          builder: (context, value, child) {
-            // switch will automatically ask you to cover all the cases of [FutureValues]
-            switch (value) {
-              // This is how we can use pattern matching in Dart, will be covered in coming blogs.
-              // Loading() => _buildLoading(),
-              // Error(message: var error) => _buildError(error),
-              // Success(value: var value) => _buildSuccess(value),
-              case Loading():
-                return _buildLoading();
-              case Error():
-                return _buildError(value.message);
-              case Success():
-                return _buildSuccess(value.value);
-            }
-          }),
-    );
-  }
-
-  Widget _buildLoading() {
-    return const Center(
-      child: CircularProgressIndicator(),
-    );
-  }
-
-  Widget _buildError(String message) {
-    return Center(
-      child: Text(message),
-    );
-  }
-
-  Widget _buildSuccess(List<Todo> todos) {
-    return ListView.builder(
-      itemCount: todos.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(
-            todos[index].title,
-            style: TextStyle(
-              decoration: todos[index].completed
-                  ? TextDecoration.lineThrough
-                  : TextDecoration.none,
-              color: todos[index].completed ? Colors.green : Colors.black,
-            ),
-          ),
-        );
-      },
     );
   }
 }
